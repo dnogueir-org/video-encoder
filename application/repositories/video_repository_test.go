6f68033ef216b10ed192fac65dba39b59e069cab
dnogueir <daniell_dng@hotmail.com>
2021-06-24 22:09:20 -0300

    feat(repositories): add video and job repositories

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,31 @@
+package repositories_test
+
+import (
+	"dnogueir-org/video-encoder/application/repositories"
+	"dnogueir-org/video-encoder/domain"
+	"dnogueir-org/video-encoder/framework/database"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, v.ID, video.ID)
+}
