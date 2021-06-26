8fe6c09c199bfb8fba4c26b0486ce4a9eb85b0ca
dnogueir <daniell_dng@hotmail.com>
2021-06-26 19:07:43 -0300

    feat(updaloadManager): add upload manager

---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,48 @@
+package services_test
+
+import (
+	"dnogueir-org/video-encoder/application/services"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func TestVideoServiceUpload(t *testing.T) {
+	video, repo := prepare()
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	bucketName := "video-encoder-bucket"
+	err := videoService.Download(bucketName)
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "video-encoder-bucket"
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(20, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, "upload completed", result)
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
